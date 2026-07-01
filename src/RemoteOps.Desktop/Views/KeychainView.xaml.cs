a2480c9d1f99e1a6b0b64d6afda33bd43a870149
Vagner <vagnerss2011@gmail.com>
2026-07-01 20:20:32 -0300

    feat(gui): views Hosts/Keychain/Logs (conteudo do rail)

---
+++ src/RemoteOps.Desktop/Views/KeychainView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace RemoteOps.Desktop.Views;
+
+public partial class KeychainView : UserControl
+{
+    public KeychainView()
+    {
+        InitializeComponent();
+    }
+}
